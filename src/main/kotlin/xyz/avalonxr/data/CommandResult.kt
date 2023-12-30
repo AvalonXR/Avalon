790136d0b8a5c3e0ef2f2274a715bb35912d71c0
Atri <atriusedm@gmail.com>
2023-12-29 22:03:54 -0500

    feat: Update CommandResult with extra functions and state classes

--- src/main/kotlin/xyz/avalonxr/data/CommandResult.kt
+++ src/main/kotlin/xyz/avalonxr/data/CommandResult.kt
@@ -2,19 +2,74 @@ package xyz.avalonxr.data
 
 import xyz.avalonxr.data.error.AvalonError
 
-class CommandResult private constructor(
-    error: AvalonError? = null,
-) : Payload<AvalonError>(error) {
+/**
+ * @author Atri
+ *
+ * A class structure used specifically in determining command execution status. This comes in several varying states,
+ * including [Success], [Failure], and [Info]. Each state represents and is useful for portraying to the application the
+ * resulting execution state of a given command. This integrates with our default command handler to display status via
+ * embed sidebar color.
+ */
+sealed class CommandResult {
 
-    fun isError(): Boolean = data != null
+    /**
+     * Represents a successful command execution. Useful for representing to the application that a command returned a
+     * successful result.
+     *
+     * @property payload The associated payload.
+     */
+    data class Success(val payload: Any?) : CommandResult()
+
+    /**
+     * Represents a failed command execution. Useful for representing to the application that a command returned a
+     * bad result.
+     *
+     * @property error The associated error object.
+     */
+    data class Failure(val error: AvalonError) : CommandResult()
+
+    /**
+     * Represents a command result which does not change regardless of execution. Useful for commands or subcommands
+     * that simply return a message.
+     *
+     * @property message The associated info message.
+     */
+    data class Info(val message: String) : CommandResult()
+
+    /**
+     * Whether the object in question is a failure state.
+     *
+     * @return True if the result is a [Failure] state.
+     */
+    fun isError(): Boolean = this is Failure
 
     companion object {
 
-        fun success(): CommandResult =
-            CommandResult()
+        /**
+         * Creates a [Success] instance.
+         *
+         * @param value The value associated with this result.
+         *
+         * @return A [Success] object containing the corresponding [value].
+         */
+        fun success(value: Any? = null): CommandResult = Success(value)
 
-        fun failure(
-            error: AvalonError
-        ) = CommandResult(error)
+        /**
+         * Creates a [Failure] instance.
+         *
+         * @param error The error associated with this result.
+         *
+         * @return A [Failure] object containing the corresponding [error].
+         */
+        fun failure(error: AvalonError): CommandResult = Failure(error)
+
+        /**
+         * Creates an [Info] instance.
+         *
+         * @param info The message associated with this result.
+         *
+         * @return An [Info] object containing the corresponding [info] message.
+         */
+        fun info(info: String): CommandResult = Info(info)
     }
 }
--- src/main/kotlin/xyz/avalonxr/data/Payload.kt
+++
@@ -1,5 +0,0 @@
-package xyz.avalonxr.data
-
-open class Payload<T>(
-    val data: T? = null
-)
